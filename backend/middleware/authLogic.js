5eff8f27e6f80f941953e15b149691fa6a5db7d3
ashmit <kumarashmitpandey051525@gmail.com>
2025-01-13 01:12:57 +0530

    modified authlogic in backend

--- backend/middleware/authLogic.js
+++ backend/middleware/authLogic.js
@@ -1,8 +1,6 @@
 const authorize=(allowAdmin=false)=> (req, res, next)=>{
     const user = req.user;
     const id = Number(req.params.userId);
-    console.log(id);
-    console.log(user.id);
     if(!id){
         return res.status(404).json({
             error: 'User id not found'
