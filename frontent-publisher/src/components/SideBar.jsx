e6935b8a55c8132d6609cfd2cdcf5a772dfe7b56
ashmit <kumarashmitpandey051525@gmail.com>
2025-01-23 17:44:18 +0530

    Add: Interactive sidebar

--- frontent-publisher/src/components/SideBar.jsx
+++ frontent-publisher/src/components/SideBar.jsx
@@ -7,22 +7,27 @@ function SideBar(){
     const {toggleSideBar} = useContext(LayoutContext);
 
     return(
-        <>
-        <section className="fixed top-0 left-0 w-[200px] bg-gray-500 h-screen pt-4">
+        <div className="flex flex-row w-screen h-screen fixed top-0 left-0">
+        <div className="w-1/2 h-screen bg-customCool-dark p-2">
             <button onClick={toggleSideBar}><CloseIcon classname={"w-16 h-16 fill-red-500"}/></button>
-            <ul>
-                <li>
+
+            <ul className="text-customWarm-light text-xl px-4 flex flex-col gap-2 divide-y-2">
+                <li className="hover:bg-customCool-light hover:text-customCool-dark">
                     <Link to={'/'}>Home</Link>
                 </li>
-                <li>
+                <li className="hover:bg-customCool-light hover:text-customCool-dark">
                     <Link to={'/create'}>create</Link>
                 </li>
-                <li>
+                <li className="hover:bg-customCool-light hover:text-customCool-dark">
                     <Link to={'/blogs'}>blogs</Link>
                 </li>
             </ul>
-        </section>
-        </>
+        </div>
+
+        <div className='flex-grow' onClick={toggleSideBar}>
+
+        </div>
+        </div>
     )
 }
 
